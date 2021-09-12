efc2658d6f12ddbc81aa49a7c320136fd3f43169
varshini <dreamvarshini@gmail.com>
2021-09-13 00:28:16 +0530

    ADDED: stack demo function

--- src/com/bridgelabz/collectionsdemo/CollectionsDemo.java
+++ src/com/bridgelabz/collectionsdemo/CollectionsDemo.java
@@ -3,6 +3,7 @@ package com.bridgelabz.collectionsdemo;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 
 public class CollectionsDemo 
@@ -13,6 +14,7 @@ public class CollectionsDemo
 	public static void main(String[] args) {
 
 		doListDemo();
+		doStackDemo();
 		
 
 	}
@@ -26,6 +28,23 @@ public class CollectionsDemo
 		list.add("Ajay");
 		
 		Iterator<String> iterator = list.iterator();
+		while (iterator.hasNext()) 
+		{
+			System.out.println(iterator.next());
+		}
+		
+	}
+	private static void doStackDemo()
+	{
+		
+		System.out.println("Stack demo: ");
+		Stack<String> stack = new Stack<String>();
+		stack.push("aaa");
+		stack.push("bbb");
+		stack.push("ccc");
+		String elementAtTop = stack.pop();
+		
+		Iterator<String> iterator = stack.iterator();
 		while (iterator.hasNext()) 
 		{
 			System.out.println(iterator.next());
